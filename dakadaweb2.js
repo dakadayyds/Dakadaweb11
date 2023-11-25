f4511ae5d693956cf4f8f8a254706f48b61bbbd7
dakadayyds <90085352+dakadayyds@users.noreply.github.com>
2023-11-25 16:26:16 +0800

    Create dakadaweb2.js

---
+++ dakadaweb2.js
@@ -0,0 +1,19 @@
+(function(Scratch){
+    function getOverlayMode () {
+        resizeBehavior === "scale" ? "scale-centered" : "manual";
+    }
+     let iframe = document.createElement("iframe");
+
+     
+  function src(){
+    const { stageWidth, stageHeight } = Scratch.vm.runtime;
+    let iframewidth=stageWidth;
+    let iframeHeight=stageHeight-50;
+    iframe.style.width = `${effectiveWidth}px`;
+    iframe.style.height = `${effectiveHeight}px`;
+  }
+       let overlay = Scratch.renderer.addOverlay(iframe,"scale");
+            Scratch.renderer._updateOverlays();
+          src()
+          Scratch.vm.on("STAGE_SIZE_CHANGED", src);
+})(Scratch);
