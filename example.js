2b93a1d7a7200f44e7621f4ca67327a5e6ffe083
dakadayyds <90085352+dakadayyds@users.noreply.github.com>
2023-12-02 22:26:16 +0800

    Update example.js

--- example.js
+++ example.js
@@ -9,13 +9,14 @@ this.webObject=class{
     return{
       index:this.index,  /*指令：open projectname or projectname@index*/
       /*testpage:this.method //open projectname@testpage*/
+      /*error500:method1*/
+      /*error404:method2*/
+      /*↑触发404或500错误时dakadaweb将调用error404或error500，如果没有定义则显示默认页面↑*/
     }
   }
     index(){
       return `<h1>hello!</h1><p>dakadaweb's example page</p>`
     }
     /*method(){return `something...`}*/
-  /*error404(){}*/
-    /*error500(){}*/
-  /*触发404或500错误时dakadaweb将调用他们，如果没有定义则显示默认页面*/
+
 }
