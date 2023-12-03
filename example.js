8afef50421b630e3dcf2b2510282170422c6bc36
dakadayyds <90085352+dakadayyds@users.noreply.github.com>
2023-12-03 12:55:26 +0800

    Update example.js

--- example.js
+++ example.js
@@ -7,23 +7,21 @@ this.webObject=class{
   Info(){
     /*获取信息*/
     return{
-      index:{HTML:this.index},  /*指令：open projectname or projectname@index，script见下方注释*/
+      index:{HTML:this.index,script:this.contentscript},  /*指令：open projectname or projectname@index，script见下方注释*/
       /*testpage:this.method //open projectname@testpage*/
-      /*error500:method1*/
-      /*error404:method2*/
-      /*↑触发404或500错误时dakadaweb将调用error404或error500，如果没有定义则显示默认页面↑*/
+      /*{error500:method1}
+      {error404:method2}
+      ↑触发404或500错误时dakadaweb将调用error404或error500，如果没有定义则显示默认页面↑*/
     }
   }
     index(){
-      this.contentscript=function(){
-              /*（必填）内嵌于网页执行的脚本（HTML代码生成完后执行），代码保密性较强*/
+        /*页面模板*/
+        return `<h1>hello!</h1><p>dakadaweb's example page</p>`;
+    }
+  contentscript(){
+          /*待html加载完毕后内嵌于网页执行的脚本，保密性较强*/
               console.log("hello")
       }
-      this.template=function(){
-        /*页面模板（必填）*/
-        return `<h1>hello!</h1><p>dakadaweb's example page</p>`;
-      }
-    }
     /*method(){return `something...`}*/
 
 }
