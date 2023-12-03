b8c29478fd75760537250395ee1d6e376c07903b
dakadayyds <90085352+dakadayyds@users.noreply.github.com>
2023-12-03 14:16:09 +0800

    Fix bug
    all

--- dakadaweb.js
+++ dakadaweb.js
@@ -24,7 +24,7 @@ class dakadawebextension{
 	}
     getInfo(){
         return{
-            id:"dakadawebextensionbeta2",
+            id:"dakadawebextension1",
             name:'dakadaweb',
             menuIconURI:image,
             blockIconURI: image,
